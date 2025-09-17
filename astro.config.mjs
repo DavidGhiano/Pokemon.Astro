c6f68c62e8ecf2eb6fcc49ed674101b05ecfbdff
David Ghiano <davidghiano@gmail.com>
2025-09-16 23:17:47 -0300

    feat: add Solid.js components for counter and favorite Pokémon functionality
    - Added Counter component with increment and decrement functionality.
    - Introduced FavoritePokemonCard and FavoritePokemons components to manage and display favorite Pokémon from local storage.
    - Updated NavBar and Title components for better navigation and consistency.
    - Created new icons for heart states (full, outline, break).
    - Refactored PokemonCard component to remove unnecessary fetch logic.
    - Enhanced Pokémon detail page with favorite toggle functionality and improved layout.
    - Updated TypeScript interfaces for better type safety.
    - Modified main layout to include NavBar and adjusted styles for better responsiveness.
    - Updated package.json to include necessary dependencies for Solid.js and icons.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import  icon  from 'astro-icon';
+
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [icon(), solidJs()],
     vite: {
         plugins: [tailwindcss()],
     },
